91e528db67a6e8e11be959678d79975caff8df17
赵建 <Z@MacBook-Pro.local>
2023-04-06 23:48:51 +1200

    adjust nav bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg'
+
 import './App.css'
-import Header from './component/Header'
+import Header from './components/Header'
 import { Container } from '@mui/material'
 
 function App() {
