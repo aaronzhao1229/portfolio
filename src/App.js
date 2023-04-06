06bfe393241e987233a96f83c1205dc1e558b05f
赵建 <Z@MacBook-Pro.local>
2023-04-06 18:39:22 +1200

    another simple change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './component/Header';
+import logo from './logo.svg'
+import './App.css'
+import Header from './component/Header'
+import { Container } from '@mui/material'
 
 function App() {
   return (
     <div className="App">
-      
       <Header />
+      <Container>Hey world!</Container>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
